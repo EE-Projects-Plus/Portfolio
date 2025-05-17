646ec42ce67942393658eaeb25df322321cc8779
Mr_E <141496204+EE-Projects-Plus@users.noreply.github.com>
2025-05-17 11:59:11 +0100

    Update data.js
    Additional content

--- data.js
+++ data.js
@@ -6,29 +6,34 @@ const fillerIntroContent = {
 
 const steps = [
   {
-    title: "1. Discover",
+    title: "1. Plan",
     description: "Understand your goals, audience, and brand direction.",
     animation: "fade-right",
   },
   {
-    title: "2. Plan",
+    title: "2. Design",
     description: "Outline features, content flow, and key design elements.",
     animation: "fade-left",
   },
   {
-    title: "3. Build",
+    title: "3. Develop",
     description: "Develop responsive, accessible code using clean structure.",
     animation: "fade-right",
   },
   {
-    title: "4. Optimize",
+    title: "4. Test",
     description: "Leverage AI tools to speed up writing, testing and QA.",
     animation: "fade-left",
   },
   {
-    title: "5. Launch",
+    title: "5. Deploy",
     description: "Deploy, test live, and support any post-launch updates.",
     animation: "fade-right",
+  },
+  {
+    title: "6. Review",
+    description: "Deploy, test live, and support any post-launch updates.",
+    animation: "fade-left",
   },
 ];
 
@@ -45,13 +50,25 @@ const projects = [
     image: "img/OR_logo.png",
     url: "https://ee-projects-plus.github.io/OtakuRarities/",
   },
-  // Add more projects as needed
+  {
+    title: "Edward Enterprise",
+    description: "A corperate group",
+    image: "img/EE_logo.png",
+    url: "https://ee-projects-plus.github.io/OtakuRarities/",
+  },
+  {
+    title: "AEDEN store",
+    description: "A store front to sell their products",
+    image: "img/AE_logo.png",
+    url: "https://ee-projects-plus.github.io/OtakuRarities/",
+  },
+  // Add more here
 ];
 
 const footerContent = {
   text: "© 2025 Matthew Robinson",
-  contactEmail: "your.email@example.com",
-  githubUrl: "https://github.com/yourusername",
-  linkedinUrl: "https://linkedin.com/in/yourprofile"
+  contactEmail: "matt.e.robinson@outlook.com",
+  githubUrl: "https://github.com/EE-Projects-Plus",
+  linkedinUrl: "https://linkedin.com/in/matthew-robinson-076320186"
 };
 
